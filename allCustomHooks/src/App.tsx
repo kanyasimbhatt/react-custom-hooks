e45cbf3e4cc6a6c1977f1942898d7e37681bb256
kanyasimbhatt <kanya.bhatt@simformsolutions.com>
2025-05-30 09:13:54 +0530

    add: add some more custom hooks

--- allCustomHooks/src/App.tsx
+++ allCustomHooks/src/App.tsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
-import Child from "./Components/Child";
-// const App = () => {
-//   const [count, setCount] = useState<number>(0);
-//   const previousValue: number | null = usePreviousValue<number>(count);
-//   console.log(previousValue);
-//   return (
-//     <>
-//       <p>{previousValue}</p>
-//       <button onClick={() => setCount(count + 1)}>Set Data</button>
-//     </>
-//   );
-// };
 
-// const App = () => {
-//   const ref = useEventListener<HTMLInputElement>("input", () =>
-//     console.log("changed")
-//   );
-//   return <input type="text" ref={ref} />;
-// };
-
-// const App = () => {
-//   const isMatched = useMediaQuery("(min-width: 768px)");
-//   return <div>{isMatched ? "True" : "False"}</div>;
-// };
+import useIsOffline from "./Hooks/useIsOffline";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  
+  const value = useIsOffline();
+ 
   return (
     <>
-      <Child value={count} />
-      <button type="button" onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      {value ? 'OFFLINE' : "ONLINE"}
     </>
   );
 };
--- allCustomHooks/src/Hooks/useIsOffline.ts
+++ allCustomHooks/src/Hooks/useIsOffline.ts
@@ -1,14 +1,29 @@
 // hook that returns true whenever user goes offline
 // const isOffline=useIsOffline()
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useIsOffline = () => {
-  const [isOffline, setIsOffline] = useState(false);
-  window.addEventListener("offline", () => {
-    setIsOffline(true);
-  });
-  return [isOffline];
+
+  const [isOffline, setIsOffline] = useState(!navigator.onLine);
+  useEffect(()=>{
+    const goOffline = () => {
+      setIsOffline(true);
+    }
+
+    const goOnline = () => {
+  
+      setIsOffline(false)
+    }
+
+    window.addEventListener("offline", goOffline);
+    window.addEventListener("online", goOnline);
+    return() =>  {
+      window.removeEventListener('offline', goOffline)
+      window.removeEventListener('online', goOnline);
+}
+  })
+  return isOffline;
 };
 
 export default useIsOffline;
