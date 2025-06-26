58f49f4b55d8bd2a62410143da7f5ce9ffd9d946
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-06-26 19:17:28 +0530

    add: add use click on outside hook and improve the code for useScrollSpy

--- allCustomHooks/src/Hooks/useClickOnOutside.ts
+++ allCustomHooks/src/Hooks/useClickOnOutside.ts
@@ -4,16 +4,25 @@
 // const ref = useRef(null);
 // useOnClickOutside(ref, () => setIsOpen(false));
 
+import { useEffect } from "react";
+
 const useClickOnOutside = <T extends HTMLElement | null>(
   node: React.RefObject<T>,
   callback: (...args: unknown[]) => unknown | void
 ) => {
-  document.addEventListener("click", (event) => {
+  const handleClick = (event: MouseEvent) => {
     if (event.target === node.current) {
       return;
     }
     callback();
-  });
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
 };
 
 export default useClickOnOutside;
--- allCustomHooks/src/Hooks/useScrollSpy.ts
+++ allCustomHooks/src/Hooks/useScrollSpy.ts
@@ -11,38 +11,27 @@ import { useEffect, useState } from "react";
 // Expected behavior:
 // As the user scrolls through the page, the corresponding nav item updates to indicate the active section.
 
-const useScrollSpy = (sectionIds: string[]) => {
+const useScrollSpy = (sectionIds: string[], offset = 0) => {
   const [visibleSection, setVisibleSection] = useState<string>();
 
   const handleScroll = () => {
     let visibleSection: string = "";
     sectionIds.map((sections: string) => {
-      const element = document.querySelector(sections);
+      const element: HTMLElement | null = document.querySelector(sections);
       if (!element) throw new Error("This particular element does not exist");
-      const rect = element.getBoundingClientRect();
 
-      const condition =
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-          (window.innerWidth || document.documentElement.clientWidth);
-
+      const condition = window.scrollY + offset >= element.offsetTop;
       if (condition) visibleSection = sections;
     });
 
-    return visibleSection;
-  };
-
-  const handleScrollChange = () => {
-    setVisibleSection(handleScroll());
+    setVisibleSection(visibleSection);
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScrollChange);
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScrollChange);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
